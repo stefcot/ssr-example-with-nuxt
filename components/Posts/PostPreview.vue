be4634aa9c6b5a75f48b822c6ecb95d6291a7777
Stephane Cottereau <scottereau.eurostaff@tf1.fr>
2019-11-04 19:11:03 +0100

    Pages, routing & love : created post preview component

---
+++ components/Posts/PostPreview.vue
@@ -0,0 +1,77 @@
+<template>
+  <nuxt-link :to="'/posts/' + id" class="post-preview">
+    <article>
+      <div
+        class="post-thumbnail"
+        :style="{
+          backgroundImage: 'url(' + require(`@/assets/img/${thumbnail}`) + ')'
+        }"
+      ></div>
+      <div class="post-content">
+        <h1>{{ title }}</h1>
+        <p>{{ previewText }}</p>
+      </div>
+    </article>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'PostPreview',
+  props: {
+    id: {
+      type: Number,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    previewText: {
+      type: String,
+      required: true
+    },
+    thumbnail: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style class="scoped">
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 0 3px #ccc;
+  background-color: white;
+  width: 100%;
+  padding: 10px;
+  margin: 20px;
+}
+.post-preview h1 {
+  font-family: 'Fjalla One', sans-serif;
+}
+a {
+  text-decoration: none;
+  color: black;
+}
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+  }
+}
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,43 +6,22 @@
       </h1>
     </section>
     <section class="featured-posts">
-      <nuxt-link :to="'/posts/' + 1" class="post-preview">
-        <article>
-          <div
-            class="post-thumbnail"
-            :style="{
-              backgroundImage:
-                'url(' +
-                require('@/assets/img/765090-most-popular-technology-background-images-1920x1080-hd-for-mobile.jpg') +
-                ')'
-            }"
-          ></div>
-          <div class="post-content">
-            <h1>Post title 1</h1>
-            <p>Preview post 1</p>
-          </div>
-        </article>
-      </nuxt-link>
-      <nuxt-link :to="'/posts/' + 2" class="post-preview">
-        <article>
-          <div
-            class="post-thumbnail"
-            :style="{
-              backgroundImage:
-                'url(' +
-                require('@/assets/img/765090-most-popular-technology-background-images-1920x1080-hd-for-mobile.jpg') +
-                ')'
-            }"
-          ></div>
-          <div class="post-content">
-            <h1>Post title 2</h1>
-            <p>Preview post 2</p>
-          </div>
-        </article>
-      </nuxt-link>
+      <post-preview
+        :id="1"
+        title="First post"
+        preview-text="Some greta explanation"
+        thumbnail="765090-most-popular-technology-background-images-1920x1080-hd-for-mobile.jpg"
+      ></post-preview>
     </section>
   </div>
 </template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+export default {
+  components: { PostPreview }
+}
+</script>
 
 <style class="scoped">
 .intro {
@@ -80,36 +59,5 @@
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-}
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 0 3px #ccc;
-  background-color: white;
-  width: 100%;
-  padding: 10px;
-  margin: 20px;
-}
-a {
-  text-decoration: none;
-  color: black;
-}
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-  }
-}
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
 }
 </style>
